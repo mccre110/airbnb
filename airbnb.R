c747d355dc60a88a0bf1b329fb50299adb2187c1
JaredLChavez <JaredLChavez@users.noreply.github.com>
2020-12-01 15:21:20 -0800

    Added cleaning code that mutated columns and removed unnecessary columns and NA's

--- airbnb.R
+++ airbnb.R
@@ -1,5 +1,4 @@
 #Estimate a model
-#hello world
 rm(list = ls())
 library(knitr)
 library("readr")
@@ -20,7 +19,42 @@ glimpse(airbnb)
 
 
 #cleaning here
-airbnb_clean <- airbnb %>% mutate(price = exp(log_price))
+
+airbnb_clean <- airbnb %>% as_tibble() %>%
+  mutate(price = exp(log_price)) %>% 
+  mutate_if(is.character, as.factor) %>% 
+  select(-id, 
+         -log_price, 
+         -amenities, 
+         -description, 
+         -first_review, 
+         -host_has_profile_pic, 
+         -host_identity_verified,
+         -host_response_rate, 
+         -last_review, 
+         -latitude,
+         -longitude,
+         -name,
+         -thumbnail_url,
+         -zipcode) 
+
+airbnb_clean <- airbnb_clean %>% filter(!is.na(price), 
+        !is.na(property_type),
+        !is.na(room_type),
+        !is.na(accommodates),
+        !is.na(bathrooms),
+        !is.na(bed_type),
+        !is.na(cancellation_policy),
+        !is.na(cleaning_fee),
+        !is.na(city),
+        !is.na(instant_bookable),
+        !is.na(neighbourhood),
+        !is.na(number_of_reviews),
+        !is.na(review_scores_rating),
+        !is.na(bedrooms),
+        !is.na(beds))
+
+          
 #should bathrooms be a factor?
 #first review should be a date
 #response rate should be a double
@@ -47,8 +81,6 @@ airbnb_test$lm_preds <- predict(lm_mod, newdata = airbnb_test)
 
 rsq(airbnb_train, log_price, lm_preds)
 rsq(airbnb_test, log_price, lm_preds)
-
-#results df
 
 
 
